caad3c570910375d9036a5045215efe510f1f2f6
Tran Quang Thai <thaidc24v7k026@vlvh.ctu.edu.vn>
2026-04-17 16:34:16 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
   plugins: [vue(), vueDevTools()],
   server: {
     port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+      },
+    },
   },
   resolve: {
     alias: {
